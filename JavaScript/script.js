3641e656245554b77b48ce5c894782bd49ad3936
Sawantkun <kumar.sawant37@gmail.com>
2022-03-11 01:33:05 +0530

    changes

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -5,7 +5,7 @@ window.addEventListener('load',()=>{
     setTimeout(()=>{
         loader.style.display="none";
         body.style.overflow="visible";
-    },1);
+    },5000);
 })
 
 
@@ -94,7 +94,7 @@ ScrollTrigger.create({
 
 // animations 
 const timeline=gsap.timeline({defaults:{duration:0.5,opacity:0}});
-timeline.to('.reveal',{stagger:0.2,y:0,ease:'SlowMo.easeOut',opacity:1,background:'transparent',delay:1})
+timeline.to('.reveal',{stagger:0.2,y:0,ease:'SlowMo.easeOut',opacity:1,background:'transparent',delay:5})
         .from('#particles-js',{opacity:0})
         .from('.Hname',{y:-100})
         .from('.mouse-scroll',{y:-50},'-=0.5')
